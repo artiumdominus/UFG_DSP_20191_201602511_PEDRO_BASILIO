ca8f33a97225c9ef89fbbdfe13dcd006a2fdded5
Pedro Basilio <artiumdominus@protonmail.com>
2019-03-26 22:53:13 -0300

    atividade 05-08

---
+++ dsp20191/src/main/java/br/com/artiumdominus/dsp2019/aulas0508/as/Main.java
@@ -0,0 +1,10 @@
+package br.com.artiumdominus.dsp2019.aulas0508.as;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ManipulaAluno.geraAluno();
+        ManipulaAluno.lerAluno();
+    }
+
+}
