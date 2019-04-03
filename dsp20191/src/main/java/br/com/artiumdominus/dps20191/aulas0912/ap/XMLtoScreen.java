7040b082368a9b28e112d2568f7fff60e685fe5f
Pedro Basilio <artiumdominus@protonmail.com>
2019-04-03 15:02:39 -0300

    leitor XML

--- dsp20191/src/main/java/br/com/artiumdominus/dps20191/aulas0912/ap/XMLtoScreen.java
+++ dsp20191/src/main/java/br/com/artiumdominus/dps20191/aulas0912/ap/XMLtoScreen.java
@@ -1,30 +1,39 @@
 package br.com.artiumdominus.dps20191.aulas0912.ap;
 
-import java.io.*;
-import java.util.*;
-import org.dom4j.*;
+import java.io.File;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 public class XMLtoScreen {
 
     public static void main(String[] args) {
+
         try {
             File inputFile = new File("src/main/resources/students.xml");
-            SAXBuilder saxBuilder = new SAXBuilder();
-            Document document = saxBuilder.build(inputFile);
+            SAXReader reader = new SAXReader();
+            Document document = reader.read(inputFile);
 
             System.out.println("Root element :" + document.getRootElement().getName());
 
             Element classElement = document.getRootElement();
 
             List<Node> nodes = document.selectNodes("/class/student");
-            System.out.println();
+            System.out.println("• • • • • • • • • •");
 
             for (Node node : nodes) {
-                System.out.println("\nCurrent Element :" + node.getName());
-                System.out.println("Student roll no : " + node.valueOf("@rollno"));
-                System.out.println("First Name : " + node.selectSingleNode("firstname").getText());
-                System.out.println("Last Name : " + node.selectSingleNode("lastname").getText());
-                System.out.println("Nick Name : " + node.selectSingleNode("nickname").getText());
+                System.out.println("\nCurrent Element :"
+                        + node.getName());
+                System.out.println("First Name : "
+                        + node.selectSingleNode("firstname").getText());
+                System.out.println("Last Name : "
+                        + node.selectSingleNode("lastname").getText());
+                System.out.println("Nick Name : "
+                        + node.selectSingleNode("nickname").getText());
             }
 
         } catch (DocumentException e) {
