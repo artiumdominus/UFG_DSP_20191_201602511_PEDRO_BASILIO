76d714964c86d544f11fc816cd8fe6bdb3f3f547
Pedro Basilio <artiumdominus@protonmail.com>
2019-04-04 18:59:21 -0300

    JSON to screen

---
+++ dsp20191/src/main/java/br/com/artiumdominus/dps20191/aulas0912/ap/JSONFilterPedro.java
@@ -0,0 +1,9 @@
+package br.com.artiumdominus.dps20191.aulas0912.ap;
+
+public class JSONFilterPedro {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ dsp20191/src/main/java/br/com/artiumdominus/dps20191/aulas0912/ap/JSONFrequencySwitcher.java
@@ -0,0 +1,4 @@
+package br.com.artiumdominus.dps20191.aulas0912.ap;
+
+public class JSONFrequencySwitcher {
+}
